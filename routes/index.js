1b67922ed74b6fead6041e207318bb44484a44c1
Adam Hurley <42122290+AdamAtomus@users.noreply.github.com>
2022-04-27 00:09:11 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(403).send('Forbidden');
+  res.render('index');
 });
 
 module.exports = router;
